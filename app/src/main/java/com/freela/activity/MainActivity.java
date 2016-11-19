794f234490204973a9ed76f069c0a6796dcb3636
Gabriel Araújo <gabrielaraujow3@gmail.com>
2016-11-18 22:51:05 -0300

    oportunidade

--- app/src/main/java/com/freela/activity/MainActivity.java
+++ app/src/main/java/com/freela/activity/MainActivity.java
@@ -11,6 +11,8 @@ import com.freela.R;
 public class MainActivity extends Activity implements View.OnClickListener {
     private Button btnEntrar;
     private Button btnRegistrar;
+    private Button btOpotundiade;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +24,30 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         btnRegistrar = (Button) findViewById(R.id.btnRegistrarUsr);
         btnRegistrar.setOnClickListener(this);
+
+        btOpotundiade = (Button) findViewById(R.id.btnOportunidade);
+        btOpotundiade.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        Intent intent = null;
 
         switch (view.getId()) {
 
             case R.id.btnEntrar:
-                Intent intent = new Intent(view.getContext(), LoginActivity.class);
+                intent = new Intent(view.getContext(), LoginActivity.class);
                 startActivity(intent);
                 break;
 
             case R.id.btnRegistrarUsr:
-                //TODO: redirecionar para Registro de usuario
+                intent = new Intent(view.getContext(), RegistroUsuarioActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btnOportunidade:
+                intent = new Intent(view.getContext(), RegistrarOportunidadeActivity.class);
+                startActivity(intent);
                 break;
 
         }
