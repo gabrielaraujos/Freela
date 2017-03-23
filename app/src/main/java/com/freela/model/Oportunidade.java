b694a5322338db900a1bf66fed5bfc6646d3e588
Gabriel Araújo <gabrielaraujow3@gmail.com>
2017-03-23 10:38:43 -0300

    O Registro de oportunidade

---
+++ app/src/main/java/com/freela/model/Oportunidade.java
@@ -0,0 +1,47 @@
+package com.freela.model;
+
+import java.util.Date;
+
+/**
+ * Created by Gabriel on 16/11/2016.
+ */
+
+public class Oportunidade {
+
+    public String titulo;
+    public String descricao;
+    public Date dtInicio;
+    public Date dtFim;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDtInicio() {
+        return dtInicio;
+    }
+
+    public void setDtInicio(Date dtInicio) {
+        this.dtInicio = dtInicio;
+    }
+
+    public Date getDtFim() {
+        return dtFim;
+    }
+
+    public void setDtFim(Date dtFim) {
+        this.dtFim = dtFim;
+    }
+}
